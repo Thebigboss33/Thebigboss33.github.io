261044754fdb492d5840c95178f338b54a2b7c86
Thebigboss33 <Thebigboss33@users.noreply.github.com>
2021-05-29 12:13:20 +0200

    Re init

--- docs/assets/js/darkmode.js
+++ docs/assets/js/darkmode.js
@@ -1,15 +1,26 @@
 var darkmode = false;
-
 function switchTheme() {
-    const content = document.querySelector('.content');
+
+    const content = document.querySelector('body');
+    const footer = document.querySelector('.footer');
+    const header = document.querySelector('.header');
+    const whiteLight = document.getElementById('whiteLight');
+    const darkLight = document.getElementById('darkLight');
+
     if(darkmode == false){
         content.style.setProperty('--themecolor', 'rgba(0, 0, 0, 0.63)');
         content.style.setProperty('--fontcolor', 'rgb(255, 255, 255)');
+        footer.style.setProperty('--themecolor', 'rgba(200, 200, 200, 0.63)');
+        header.style.setProperty('--themecolor', 'rgba(200, 200, 200, 0.63)');
+        whiteLight.classList.add('hidden')
+        darkLight.classList.remove('hidden');
         darkmode = true;
     }else
     {
         content.style.removeProperty('--themecolor');
         content.style.removeProperty('--fontcolor');
+        darkLight.classList.add('hidden')
+        whiteLight.classList.remove('hidden');
         darkmode = false;
     }
 }
